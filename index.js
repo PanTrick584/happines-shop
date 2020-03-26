44e4480169c3e97eace4412a677519ab13e76224
PanTrick584 <53709752+PanTrick584@users.noreply.github.com>
2020-03-26 15:57:09 +0100

    some functionality done, now i should clean up this mess

--- index.js
+++ index.js
@@ -1,21 +1,80 @@
-const happinesElements = document.getElementById('container');
-const startContainer = document.getElementById('start-container');
-const happySpans = document.querySelectorAll('#start-header span')
+var domElement = {};
+
+domElement.mainContainer = document.getElementById('main-container');
+domElement.startContainer = document.getElementById('start-container');
+const happySpans = document.querySelectorAll('#start-header span');
+const container = document.getElementById('elements-container')
+const addElement = document.getElementById('add-element')
 
 const enterButton = document.getElementById('enter-button')
 
 
+
+var happines = [
+    {name: 'sweets', id: 1},
+    {name: 'party', id: 2},
+    {name: 'music', id: 3},
+    {name: 'people', id: 4},
+    {name: 'challenge', id: 5}
+]
 //Functions
 function showShop(){
-    startContainer.style.display = "none"
-    happinesElements.style.display = "flex";
+    domElement.startContainer.style.display = "none"
+    domElement.mainContainer.style.display = "flex";
+    happinesEntrance();
 }
+
+function happinesEntrance(){
+    console.log(happines)
+    happines.forEach( el => {
+        let button = document.createElement('button')
+        button.innerText = el.name;
+        button.id = el.id;
+        button.setAttribute('class', 'happy-element')
+        container.insertBefore(button, addElement)
+        console.log(button)
+
+    })
+
+}
+
+function addHappines(){
+    let happyDiv = document.createElement('div')
+    let inputName = document.createElement('input')
+    let buttonSubmit = document.createElement('button')
+
+
+    buttonSubmit.classList.add('header-button')
+    inputName.classList.add('add-element-input')
+    inputName.setAttribute('placeholder', "happines?")
+
+    happyDiv.setAttribute('class', "happyDiv")
+    happyDiv.appendChild(inputName);
+    happyDiv.appendChild(buttonSubmit);
+
+
+
+    addElement.classList.add('add-element')
+    addElement.appendChild(happyDiv)
+    addElement.removeEventListener('click', addHappines);
+
+    buttonSubmit.addEventListener('click', updateHappines)
+}
+
+function updateHappines(){
+    if(dupa){
+
+    }
+};
+
 //Events
 enterButton.addEventListener('click', showShop)
+addElement.addEventListener('click', addHappines)
 
-happySpans.forEach(span => {
-    Array.from(span.innerHTML, letter => {
-        console.log(letter)
-    })
-    
-})
+// // For some fun later
+// happySpans.forEach(span => {
+//     Array.from(span.innerHTML, letter => {
+//         console.log(letter)
+//     })
+
+// })
