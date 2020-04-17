63ed702b6ee5645192e3224ab51b110c0937efd9
PanTrick584 <53709752+PanTrick584@users.noreply.github.com>
2020-04-17 09:51:57 +0200

    there is a good hope for this

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-var domElement = {};
 
-domElement.mainContainer = document.getElementById('main-container');
-domElement.startContainer = document.getElementById('start-container');
-domElement.happySpans = document.querySelectorAll('#start-header span');
-domElement.elementsContainer = document.getElementById('elements-container')
-domElement.addElement = document.getElementById('add-element')
+const mainContainer = document.getElementById('main-container');
+const startContainer = document.getElementById('start-container');
+const elementsContainer = document.getElementById('elements-container');
 
-domElement.enterButton = document.getElementById('enter-button')
-domElement.createElement = document.getElementById('create-element')
+const happySpans = document.querySelectorAll('#start-header span');
+const addElement = document.getElementById('add-element');
 
-domElement.createInput = document.querySelector('.create-element input')
-domElement.createButton = document.querySelector('.create-element button')
+const enterButton = document.getElementById('enter-button');
+const createElement = document.getElementById('create-element');
+
+const createInput = document.querySelector('.create-element input');
+const createButton = document.querySelector('.create-element button');
+
+// Create DOM elements
+const happyButtonsContainer = document.createElement('div');
+happyButtonsContainer.setAttribute('class', 'happyButtonsContainer')
+elementsContainer.appendChild(happyButtonsContainer);
 
 
 
@@ -21,59 +26,83 @@ var happines = [
     {name: 'people', id: 4},
     {name: 'challenge', id: 5}
 ]
+
+var happinesElements = [
+    {id: 'sweets',
+        sweets: [
+            {name: 'chocolate', price: 8},
+            {name: 'candy', price: 2},
+            {name: 'cookie', price: 3}
+
+        ]
+
+       
+    },
+    {id: 'party',
+    
+    }
+]
+
 //Functions
+
+// Event to show entire shop after click 
 function showShop(){
-    domElement.startContainer.style.display = "none"
-    domElement.mainContainer.style.display = "flex";
+    startContainer.style.display = "none"
+    mainContainer.style.display = "flex";
     happinesEntrance();
 }
 
-function createHappines(el){
-
-    domElement.buttonHappines = document.createElement('button')
-    domElement.buttonHappines.innerText = el.name;
-    domElement.buttonHappines.id = el.id;
-    domElement.buttonHappines.setAttribute('class', 'happy-element')
-    domElement.elementsContainer.insertBefore(domElement.buttonHappines, domElement.addElement)
-}
-
+// Loop for creare elements form 'happy' array
 function happinesEntrance(){
 
-    happines.forEach( el => { createHappines(el)})
+    happines.forEach( el => { 
+        let buttonHappines = document.createElement('button')
+        buttonHappines.innerText = el.name;
+        buttonHappines.id = el.name;
+        buttonHappines.setAttribute('class', 'happy-element')
+        happyButtonsContainer.appendChild(buttonHappines)
+    })
+    openSection();
 
 }
 
-// function addHappines(){
-    
-//     domElement.createElement.style.display = "flex";
-//     domElement.createButton.addEventListener('click', pushHappines)
-    
-    
-// }
-// function pushHappines(){
 
-//     const newElement = {
-//         name: domElement.createInput.value,
-//         id: happines.length +1
-//     }
+function createShopStuff(button) {
 
-//     happines.push(newElement)
-//     domElement.createElement.style.display = "none";
-    
-//     createHappines(newElement)
-// }
+    let shopStuff = document.createElement('div');
+    let shopStuffH = document.createElement('h1');
+    shopStuffH.innerText = button.id;
+
+    console.log(button.id)
+    happinesElements.forEach(el => {
+        // console.log(el)
+        if(button.id === el.id){
+            el[el.id].forEach(el => {
+                console.log(el)
+                shopStuff.innerHTML += el.name;
+            })
+        }
+    })
+
+    elementsContainer.appendChild(shopStuffH);
+    elementsContainer.appendChild(shopStuff);
+}
 
 
+function openSection() {
+    let buttonHappinesAll = document.querySelectorAll('.happy-element');
+    console.log(buttonHappinesAll)
+    buttonHappinesAll.forEach(button => {
+        button.addEventListener('click', e => {
+           console.log( e.target.id)
+            happyButtonsContainer.style.display = 'none';
+            createShopStuff(button);
+        })
+    })
+
+}
 
 //Events
-domElement.enterButton.addEventListener('click', showShop)
-domElement.addElement.addEventListener('click', addHappines)
 
-// // For some fun later
-// happySpans.forEach(span => {
-//     Array.from(span.innerHTML, letter => {
-//         console.log(letter)
-//     })
-
-// })
-console.log(domElement)
+// Enter to shop
+enterButton.addEventListener('click', showShop)
