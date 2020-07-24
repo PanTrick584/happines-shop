7c8eae27bbdfb83df7eb3dd7e58c4aab0fbf0dc2
PanTrick584 <53709752+PanTrick584@users.noreply.github.com>
2020-07-24 18:03:40 +0200

    lightbox section done

--- index.js
+++ index.js
@@ -1,90 +1,12 @@
 // DOM ELEMENTS
 const sliderTextDOM = document.getElementById("sliderText");
 const sliderBtnDOM = document.querySelectorAll(".slider__btn");
-// OLD
-const mainContainer = document.getElementById('main-container');
-const startContainer = document.getElementById('start-container');
-const elementsContainer = document.getElementById('elements-container');
-
-const happySpans = document.querySelectorAll('#start-header span');
-const addElement = document.getElementById('add-element');
-
-const enterButton = document.getElementById('enter-button');
-const createElement = document.getElementById('create-element');
-
-const createInput = document.querySelector('.create-element input');
-const createButton = document.querySelector('.create-element button');
-
-// Spans from first main page
-const spanHighlights = document.querySelectorAll('.start-header span')
-
-// Create DOM elements
-const happyButtonsContainer = document.createElement('div');
-happyButtonsContainer.setAttribute('class', 'happyButtonsContainer')
-elementsContainer.appendChild(happyButtonsContainer);
-
-
-
-var happines = [
-    {name: 'sweets', id: 1},
-    {name: 'party', id: 2},
-    {name: 'music', id: 3},
-    {name: 'people', id: 4},
-    {name: 'challenge', id: 5}
-]
-
-var happinesElements = [
-    {
-        id: 'sweets',
-        sweets: [
-            {name: 'chocolate', price: 8},
-            {name: 'candy', price: 2},
-            {name: 'cookie', price: 3},
-            {name: 'Icecream', price: 2}
-        ]
-    },
-    {
-        id: 'party',
-        party: [
-            {name: 'Disco', price: 60},
-            {name: 'Festival', price: 120},
-            {name: 'Home Dancce', price: 7},
-            {name: 'Cameral Concert', price: 15},
-            {name: 'Singing under shower', price: 2.3},
-            {name: 'Acoustic plener concert', price: 8}
-        ]
-    },
-    {
-        id: 'music',
-        music: [
-            {name: 'Rock', price: 12},
-            {name: 'Metal', price: 12},
-            {name: 'Blues', price: 12},
-            {name: 'Jazz', price: 12},
-            {name: 'Folk', price: 12},
-            {name: 'Pop', price: 12},
-            {name: 'Regge', price: 12},
-            {name: 'Ska', price: 12},
-            {name: 'Acoustic Covers of Everything', price: 12},
-            {name: 'Dom Metal', price: 12}
-        ]
-    },
-    {
-        id: 'people',
-        people: [
-            {name: 'Girlfriend', price: 'infinity'},
-            {name: 'Boyfriend', price: 'some sex'},
-            {name: 'Friend', price: 'some time'},
-            {name: 'Domina', price: 1200},
-            {name: 'Annoying Neighbour', price: 'free'}
-        ]
-    }
-]
+// lightbox
+const lightboxDOM = document.getElementById("lightbox");
+const popupDOM = document.getElementById("popup");
 
 //Functions
-
 // SLIDER
-
 // slider sentences container
 const smartSentences = {
     one: { id: 1,
@@ -121,92 +43,96 @@ function slider() {
         
 };
 
-// Function to show entire shop after click 
-function showShop(){
-    startContainer.style.display = "none"
-    mainContainer.style.display = "flex";
-    happinesEntrance();
+//  LIGHTBOX
+const photos = [
+    "https://c.pxhere.com/photos/c7/00/halloween_ghosts_happy_halloween_ghost_autumn_october_mood_cute-791236.jpg!d",
+    "https://c.pxhere.com/photos/32/0d/hands_love_couple_together_fingers_people_family_human-1094128.jpg!d",
+    "https://c.pxhere.com/photos/30/1f/girl_sunlight_field_running_happy-99184.jpg!d",
+    "https://c.pxhere.com/photos/ef/bc/tic_tac_toe_love_heart_play_ankreuzen_strategy_game_two_people_two_people_strategy_game-372665.jpg!d",
+    "https://c.pxhere.com/photos/db/40/yellow_wall_building_architecture_girl_woman_people_silhouette-698682.jpg!d",
+    "https://c.pxhere.com/photos/3e/41/family_pier_man_woman_children_happy_holiday_silhouette-764588.jpg!d"
+];
+
+function closeLightbox() {
+
+    let btnLightbox = document.querySelector(".lightbox__button");
+    btnLightbox.addEventListener("click", () => {
+        popupDOM.style.display = "none";
+    });
 }
 
-// Loop throu first page spans to highlight them
-function spanColorChanger() {
-    Array.from(spanHighlights, span => {
-       if(span.style.color !== 'red'){
-        setInterval(()=>{
-            span.style.color = 'red';
-            // spanColorChanger();
-        }, 1500)
-       } else {
-        setInterval(()=>{
-            span.style.color = 'red';
-        }, 1500)
-        // spanColorChanger();
-       }
-    })
-}
-spanColorChanger();
-
-// Loop for creare elements form 'happy' array
-function happinesEntrance(){
-
-    happines.forEach( el => { 
-        let buttonHappines = document.createElement('button')
-        buttonHappines.innerText = el.name;
-        buttonHappines.id = el.name;
-        buttonHappines.setAttribute('class', 'happy-element')
-        happyButtonsContainer.appendChild(buttonHappines)
-    })
-    openSection();
-
-}
-
-
-function createShopStuff(button) {
-
-    let shopStuff = document.createElement('div');
-    let shopStuffH = document.createElement('h1');
-    shopStuffH.innerText = button.id;
-
-    console.log(button.id)
-    happinesElements.forEach(el => {
-        // console.log(el)
-        if(button.id === el.id){
-            el[el.id].forEach(el => {
-                console.log(el)
-                let shopStuffEl = document.createElement('div');
-                shopStuffEl.innerHTML += el.name;
-                shopStuff.appendChild(shopStuffEl);
-            })
+function generatePopup(photo) {
+    // box
+    let box = document.createElement("div");
+    box.classList.add("lightbox__popup-box");
+    // box elements
+    let screen = document.createElement("div");
+    let queue = document.createElement("div");
+    // button close
+    let button = document.createElement("button");
+    button.classList.add("lightbox__button");
+    let span = document.createElement("span");
+    span.classList.add("lightbox__button-span");
+    // box elements class
+    screen.classList.add("lightbox__popup-screen");
+    queue.classList.add("lightbox__popup-queue");
+    // screen innerHTML
+    screen.innerHTML = `<img class="lightbox__img lightbox__img-popup lightbox__img-cursor" src="${photo}"></img>`;
+    //queue innerHTML
+    photos.forEach( img => {
+        if(img !== photo) {
+        queue.innerHTML += `<div class="lightbox__popup-queue--el">
+            <img class="lightbox__img lightbox__img-popup" src="${img}"></img>
+        </div>`;
         }
-    })
+    }) 
+    
+    button.appendChild(span);
 
-    elementsContainer.appendChild(shopStuffH);
-    elementsContainer.appendChild(shopStuff);
+    box.appendChild(screen);
+    box.appendChild(queue);
+    box.appendChild(button);
+
+    popupDOM.appendChild(box);
+    closeLightbox();
+    grabImages();
 }
 
+function grabImages() {
 
-function openSection() {
-    let buttonHappinesAll = document.querySelectorAll('.happy-element');
-    console.log(buttonHappinesAll)
-    buttonHappinesAll.forEach(button => {
-        button.addEventListener('click', e => {
-           console.log( e.target.id)
-            happyButtonsContainer.style.display = 'none';
-            createShopStuff(button);
+    
+    let images = document.querySelectorAll(".lightbox__img");
+    images.forEach( img => {
+        let photo = img.src;
+        img.addEventListener("click", () => {
+            popupDOM.style.display = "block";
+            popupDOM.innerHTML = "";
+            generatePopup(photo);
         })
     })
+}
 
+function createImgs() {
+    
+    let box = document.createElement("div");
+    box.classList.add("lightbox__box");
+
+    photos.forEach( photo => {
+        box.innerHTML += 
+        `<img class="lightbox__img" src="${photo}"></img>`;
+    })
+    lightboxDOM.appendChild(box);
+    grabImages();
 }
 
 //Events
 sliderBtnDOM.forEach( btn => {
-    console.log(btn)
     btn.addEventListener("click", slider)
 })
 // document loaded
 window.addEventListener('DOMContentLoaded', ()=>{
 
     slider();
-    
+    createImgs();
 
 })
